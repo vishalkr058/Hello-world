038c5b4ce1097d047877594d30913fec7754919a
vishal <svishal724@gmail.com>
2018-10-13 00:07:38 +0530

    Add code to palindrome of integer
    Add code of palindrome of integer without using any extra space.

--- C++/Palidrome.cpp
+++ C++/Palidrome.cpp
@@ -1,26 +1,61 @@
 #include <iostream>
 using namespace std;
 
+
+
+bool pal(int);
+
+bool pal(int num)
+{
+    int div = 1;
+    while (num / div >= 10)
+        div = div * 10;
+    
+    while(num != 0)
+    {
+        int first = num / div;
+        int last = num % 10;
+        
+        if (first != last)
+            return false;
+        
+        num = (num % div) / 10;
+        
+        div = div / 100;
+    }
+    return true;
+}
+
 int main()
 {
-    int a,num,b,s=0;
-    cout << "\nPlease enter a number";
+    int num;
+    
+    cout << "enter a number";
     cin >> num;
-    b = num;
-    rev=0
-    while( num > 0)
-    {
-    rem = num % 10;
-     rev = (rev * 10) + rem;
-    num= num / 10;
-   
-    }
-   
-    if( b == rev){
-    cout << "\nit is a palindrome" << endl;
-    }
-    else{
-    cout << "\nit is not a palindrome" << endl;
-    }
+    
+    pal(num) ? cout << "it is a palindrome": cout << "it is not a palindrome";
+    
     return 0;
+    
+    // palindrome of integer with space 
+    
+    //cout << "\nPlease enter a number";
+    //cin >> num;
+    //b = num;
+    //rev=0
+    //while( num > 0)
+    //{
+    //rem = num % 10;
+    // rev = (rev * 10) + rem;
+   // num= num / 10;
+   
+    //}
+   
+    //if( b == rev){
+    //cout << "\nit is a palindrome" << endl;
+   // }
+   // else{
+   // cout << "\nit is not a palindrome" << endl;
+   // }
+   
 }
