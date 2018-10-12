4322a7b19e25cbc9df6f89918ede46e915ff99a6
vishal <svishal724@gmail.com>
2018-10-13 00:15:56 +0530

    Add new code to palindrome.cpp
    Add code of palindrome of integer without any extra space.

--- C++/Palidrome.cpp
+++ C++/Palidrome.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
 using namespace std;
 
+bool pal(int);
+
+bool pal(int num)
+{
+    int div = 1;
+    while (num / div >= 10)
+        div = div * 10;
+    
+    while(num != 0)
+    {
+        int first = num / div;
+        int last = num % 10;
+        
+        if (first != last)
+            return false;
+        
+        num = (num % div) / 10;
+        
+        div = div / 100;
+    }
+    return true;
+}
+
 int main()
+{
+    int num;
+    
+    cout << "enter a number";
+    cin >> num;
+    
+    pal(num) ? cout << "it is a palindrome": cout << "it is not a palindrome";
+    
+    return 0;
+    
+}
+
+
+// code for palindrome with extra space 
+
+/*int main()
 {
     int a,num,b,s=0;
     cout << "\nPlease enter a number";
@@ -24,3 +63,4 @@ int main()
     }
     return 0;
 }
+*/
